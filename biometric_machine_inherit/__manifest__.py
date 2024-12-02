6c51795d2b38225dc1831283fa3cdecafe193a7c
dolverac <dolverac@gmail.com>
2024-12-01 19:12:18 -0600

    Actualizacion ZK
    Entradas y Salidas Zk

---
+++ biometric_machine_inherit/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Biometric Device Integration Inheritance',
+    'version': '16.0.0.1',
+    'category': 'stock',
+    'author': 'Ing. Alejandro Garcia Magaña',
+    'website': '',
+    'license': 'LGPL-3',
+    'summary': 'Biometric Device Integration Inheritance',
+
+    'depends': [
+        'hr_biometric_machine_zk',
+    ],
+    'data': [
+    ],
+    'demo': [],
+    'external_dependencies': {
+    },
+    'assets': {
+    },
+    'support': '',
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+}
