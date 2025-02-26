237d58c6d184355fd66188d897af25e94c1ccd85
dolverac <dolverac@gmail.com>
2025-02-26 09:14:28 -0600

    Asistencia Hoja de Horas
    Modulo para importar asistencia a Hoja de horas

---
+++ attendance_timesheet/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Hojas de horas en asistencias',
+    'version': '16.0.0.1',
+    'category': 'stock',
+    'author': 'Ing. Alejandro Garcia Magaña',
+    'website': '',
+    'license': 'LGPL-3',
+    'summary': 'Hojas de horas en asistencias',
+
+    'depends': [
+        'hr_attendance',
+        'hr_timesheet',
+        'project',
+        'website_studio',
+    ],
+    'data': [
+        'security/hr_attendance_security.xml',
+        'views/hr_attendance_views.xml',
+    ],
+    'demo': [],
+    'external_dependencies': {
+    },
+    'assets': {
+    },
+    'support': '',
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+}
